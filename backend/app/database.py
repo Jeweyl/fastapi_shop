44a7d3d47ddbc837b21964bf228e3d5982387e26
Jewey <andrey.pr.developer@gmail.com>
2025-12-12 21:54:57 +0300

    add  models for category and products

--- backend/app/database.py
+++ backend/app/database.py
@@ -12,3 +12,13 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
---
+++ backend/app/models/product.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from datatime import datatime
+
+from .base import Base
+from .category import Category
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, que=True, nullable=False, index=True)
+    slug = Column(String, unique=True, nullable=False, index=True)
+    description = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    image_url = Column(String)
+    category_id = Column(Integer, nullable=False)
+    category = relationship("Category", back_populates="products")
+
+    def __repr__(self):
+        return f"Product(id={self.id}, name={self.name}, price={self.price} )"
